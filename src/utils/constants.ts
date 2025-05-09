5be9c74513660f13ae7b1f07d337bf4e5a35be5b
Kingsley Victor <javaprodigy56@gmail.com>
2025-05-09 21:31:45 +0100

    chore: update rpc url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,7 +14,7 @@ export const BD_ONE = new BigDecimal('1');
 export const BD_ZERO = new BigDecimal('0');
 
 export const RPC_URLS: { [key in Chains]: string } = {
-  [Chains.MON_TESTNET]: 'https://10143.rpc.hypersync.xyz',
+  [Chains.MON_TESTNET]: 'https://testnet-rpc.monad.xyz',
 };
 
 // Oracles
